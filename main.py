fa797a67c64dad68d1d5ffc3dcade2c8b4d2865a
thatbeer <88705394+thatbeer@users.noreply.github.com>
2022-10-14 00:07:52 +0700

    add post and user function

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from db import models
+from db.database import engine
+from router import user , post
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+@app.get("")
+def root():
+    return "Hello function"
+
+app.include_router(user.router)
+app.include_router(post.router)
+
+models.Base.metadata.create_all(engine)
+
+app.mount('/images',StaticFiles(directory='images'), name='images')
---
+++ router/post.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, status, UploadFile, File
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm import Session
+from router.schemas import PostBase, PostDisplay
+from db.database import get_db
+from db import db_post
+from typing import List
+import shutil
+import string
+import random
+
+router = APIRouter(
+    prefix='/post'
+    ,tags=['post']
+)
+
+image_url_types = ['absolute','relative']
+
+@router.post('')
+def create(request: PostBase , db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="parameter image_url_type can ony takes values as 'abosulte' or 'relative'.")
+    if request.creator_id == 0:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="parameter creator id cannot be value 0.")
+    return db_post.create(db , request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+  letters = string.ascii_letters
+  rand_str = ''.join(random.choice(letters) for i in range(6))
+  new = f'_{rand_str}.'
+  filename = new.join(image.filename.rsplit('.', 1))
+  path = f'images/{filename}'
+
+  with open(path, "w+b") as buffer:
+    shutil.copyfileobj(image.file, buffer)
+  
+  return {'filename': path}
+
+@router.get('/delete/{id}')
+def delete(id: int, db: Session = Depends(get_db)):
+  return db_post.delete(db, id)
