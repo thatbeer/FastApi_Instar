2bae1e41369c2069823cfe5d7998425d36366c42
thatbeer <6130812021@student.chula.ac.th>
2022-10-14 10:44:46 +0700

    add CORS

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from router import user , post , comment
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -16,6 +17,21 @@ app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(comment.router)
 
+origins = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 models.Base.metadata.create_all(engine)
 
 app.mount('/images',StaticFiles(directory='images'), name='images')
+
